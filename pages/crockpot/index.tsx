2b6b8836f77d04dd8e9de3e45aa7f9930bc083b6
xxgunnery <pbdantonio@gmail.com>
2022-11-08 21:51:39 -0500

    Build error

--- pages/crockpot/index.tsx
+++ pages/crockpot/index.tsx
@@ -530,21 +530,6 @@ const Home = () => {
             }
         });
 
-        jQuery(function () {
-            jQuery('a[href*="#"]:not([href="#"])').click(function () {
-                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                    var target = jQuery(this.hash);
-                    target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
-                    if (target.length) {
-                        jQuery('html, body').animate({
-                            scrollTop: target.offset().top
-                        }, 1000);
-                        return false;
-                    }
-                }
-            });
-        });
-
         jQuery(window).click(
             function (event) {
                 var options = jQuery("#dropdownOptions");
